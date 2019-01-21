ae82b2b0f0323d998c7ac2565947bd6011470590
Kevin David Cabrera Rodríguez <kevincabrerarodriguez@gmail.com>
2019-01-21 15:08:16 +0000

    - El cliente realiza la conexion inicial con los datos basicos - Ahora la clase Player properties en lugar de datos primitivos, para que puedan ser bindeados

--- src/dad/javaspace/Main.java
+++ src/dad/javaspace/Main.java
@@ -1,19 +1,71 @@
 package dad.javaspace;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Map;
 
 import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.entity.Entities;
 import com.almasb.fxgl.input.*;
 import com.almasb.fxgl.settings.GameSettings;
 
+import dad.javaspace.objects.Player;
+import dad.javaspace.physics.Physics;
 import javafx.scene.input.KeyCode;
 import javafx.scene.text.Text;
 
 public class Main extends GameApplication {
-	//
+	
+	private String version = "0.0.1";
+
+	private static String ip = "10.2.2.64", name = "jugador_test", skin = "0";
+	private static int id;
+
+	private static InputStreamReader flujoEntrada;
+	private static OutputStreamWriter flujoSalida;
+
+	private static Player player = new Player();
+
+	private static Physics physics = new Physics();
 
 	public static void main(String[] args) {
+
+		try {
+			Socket sk;
+			
+			sk = new Socket(ip, 2000);
+			
+			Thread.sleep(3000);
+			
+			flujoEntrada = new InputStreamReader(sk.getInputStream());
+			
+			flujoSalida = new OutputStreamWriter(sk.getOutputStream());
+			
+			flujoSalida.write(name + "," + skin + "\n");
+			
+			flujoSalida.flush();
+			
+			System.out.println("nombre enviado");
+			
+			id = flujoEntrada.read();
+			
+			System.out.println("id recibida");
+			
+			System.out.println(id);
+			
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		launch(args);
+
+
 	}
 
 	@Override
@@ -21,71 +73,71 @@ public class Main extends GameApplication {
 		settings.setWidth(600);
 		settings.setHeight(600);
 		settings.setTitle("JavaSpace");
-		settings.setVersion("0.0.1");
+		settings.setVersion(version);
 	}
-	
+
 	@Override
 	protected void initInput() {
-	    Input input = getInput();
+		Input input = getInput();
 
-	    input.addAction(new UserAction("Rotate Right") {
-	        @Override
-	        protected void onAction() {
-	            // Enviar input al servidor
-	        }
-	    }, KeyCode.D);
-	    
-	    input.addAction(new UserAction("Rotate Left") {
-	        @Override
-	        protected void onAction() {
-	            // Enviar input al servidor
-	        }
-	    }, KeyCode.A);
-	    
-	    input.addAction(new UserAction("Add thrust") {
-	        @Override
-	        protected void onAction() {
-	            // Enviar input al servidor
-	        }
-	    }, KeyCode.W);
+		input.addAction(new UserAction("Rotate Right") {
+			@Override
+			protected void onAction() {
+				// Enviar input al servidor
+			}
+		}, KeyCode.D);
 
-	    input.addAction(new UserAction("Remove thrust") {
-	        @Override
-	        protected void onAction() {
-	            // Enviar input al servidor
-	        }
-	    }, KeyCode.S);
-	    
-	    input.addAction(new UserAction("Shoot") {
-	        @Override
-	        protected void onAction() {
-	            // Enviar input al servidor
-	        }
-	    }, KeyCode.SPACE);
-	    
+		input.addAction(new UserAction("Rotate Left") {
+			@Override
+			protected void onAction() {
+				// Enviar input al servidor
+			}
+		}, KeyCode.A);
+
+		input.addAction(new UserAction("Add thrust") {
+			@Override
+			protected void onAction() {
+				// Enviar input al servidor
+			}
+		}, KeyCode.W);
+
+		input.addAction(new UserAction("Remove thrust") {
+			@Override
+			protected void onAction() {
+				// Enviar input al servidor
+			}
+		}, KeyCode.S);
+
+		input.addAction(new UserAction("Shoot") {
+			@Override
+			protected void onAction() {
+				// Enviar input al servidor
+			}
+		}, KeyCode.SPACE);
+
 	}
-	
+
 	@Override
 	protected void initUI() {
-	    Text textPixels = new Text();
-	    textPixels.setTranslateX(50); // x = 50
-	    textPixels.setTranslateY(100); // y = 100
+		Text textPixels = new Text();
+		textPixels.setTranslateX(50); // x = 50
+		textPixels.setTranslateY(100); // y = 100
 
-	    getGameScene().addUINode(textPixels); // add to the scene graph
+		getGameScene().addUINode(textPixels); // add to the scene graph
 	}
-	
+
 	@Override
 	protected void initGameVars(Map<String, Object> vars) {
-	    vars.put("pixelsMoved", 0);
+		vars.put("pixelsMoved", 0);
 	}
 
 	@Override
 	protected void initGame() {
 		super.initGame();
+
 //		player = Entities.builder().at(300, 300).viewFromNode(new ImageView("/dad/javaspace/resources/images/player.png"))
 //				.buildAndAttach(getGameWorld());
-		
-		
+
 	}
 
 }
--- src/dad/javaspace/objects/Player.java
+++ src/dad/javaspace/objects/Player.java
@@ -1,28 +1,41 @@
 package dad.javaspace.objects;
 
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+
 public class Player extends RigidBody {
 
-	private double thrust = 0;
-	private int lives = 3;
+	private DoubleProperty thrust = new SimpleDoubleProperty(this, "thrust", 0);
+	private IntegerProperty lives = new SimpleIntegerProperty(this, "lives", 3);
 
 	public Player() {
 
 	}
 
-	public double getThrust() {
-		return thrust;
+	public final DoubleProperty thrustProperty() {
+		return this.thrust;
 	}
 
-	public void setThrust(double thrust) {
-		this.thrust = thrust;
+	public final double getThrust() {
+		return this.thrustProperty().get();
 	}
 
-	public int getLives() {
-		return lives;
+	public final void setThrust(final double thrust) {
+		this.thrustProperty().set(thrust);
 	}
 
-	public void setLives(int lives) {
-		this.lives = lives;
+	public final IntegerProperty livesProperty() {
+		return this.lives;
+	}
+
+	public final int getLives() {
+		return this.livesProperty().get();
+	}
+
+	public final void setLives(final int lives) {
+		this.livesProperty().set(lives);
 	}
 
 }
