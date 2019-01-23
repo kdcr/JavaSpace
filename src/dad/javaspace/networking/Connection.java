42326510f4d016ef840e340de6bc103db92b62ab
michaelTP1 <michaeljtp1@gmail.com>
2019-01-23 14:01:50 +0000

    avances en el servidor
    avances en el servidor (avances en el servidor);

--- src/dad/javaspace/networking/Connection.java
+++ src/dad/javaspace/networking/Connection.java
@@ -5,46 +5,40 @@ import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 
 import com.almasb.fxgl.entity.Entity;
 
-public class Connection extends Thread{
-	
+public class Connection extends Thread {
+
 	private Socket sk;
 	private String[] itemState;
+	
+	private boolean isDamaged;
 
 	private int identity;
-	
-	
-	
-	
+
 	private String[] nombreSkin;
 	private String nombre, skin;
 
-	
-	private static  ArrayList<Connection> connectionsArray;
-	
-	private static CyclicBarrier barrera =new CyclicBarrier(Server.NPLAYERS);
+	private static ArrayList<Connection> connectionsArray;
 
+//	private static CyclicBarrier barrera = new CyclicBarrier(Server.NPLAYERS);
 	
-	private static Entity player=new Entity();
-	
-	Scanner entrada ;
+	private static ArrayList<Entity> bulletsArray=new ArrayList<Entity>();
+
+	private static Entity player = new Entity();
+
+	Scanner entrada;
 	OutputStreamWriter salida;
-	
-	
-	
-	
+
 	public Connection(Socket sk, int id, ArrayList<Connection> connectionsArray) throws IOException {
-		this.sk=sk;
-		this.identity=id;
-		this.connectionsArray=connectionsArray;
-		
-		
-		entrada= new Scanner(sk.getInputStream(), "UTF8");
-		salida=new OutputStreamWriter(sk.getOutputStream(), "UTF8");
+		this.sk = sk;
+		this.identity = id;
+		Connection.connectionsArray = connectionsArray;
+
+		entrada = new Scanner(sk.getInputStream(), "UTF8");
+		salida = new OutputStreamWriter(sk.getOutputStream(), "UTF8");
 //		
 //		try {
 //			barrera.await();
@@ -53,43 +47,47 @@ public class Connection extends Thread{
 //		} catch (BrokenBarrierException e) {
 //			e.printStackTrace();
 //		}
-		
+
 	}
-	
+
 	@Override
 	public void run() {
 		super.run();
-		
-		
-		nombreSkin=entrada.nextLine().toString().split(",");
-		
-		nombre=nombreSkin[0];
-		skin=nombreSkin[1];
-		
-		System.out.println("nombre: "+nombreSkin[0]+" skin: "+nombreSkin[1]+"\n");
-		
+
+		nombreSkin = entrada.nextLine().toString().split(",");
+
+		nombre = nombreSkin[0];
+		skin = nombreSkin[1];
+
+		System.out.println("nombre: " + nombreSkin[0] + " skin: " + nombreSkin[1] + "\n");
+
 		try {
-				salida.write(identity);
-				salida.flush();
-				salida.write(Server.getPlayers());
-				salida.flush();
-				
+			salida.write(identity);
+			salida.flush();
+			salida.write(Server.getPlayers());
+			salida.flush();
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		
-		
-		while(true){
-			
-			itemStateString=entrada.nextLine().toString();
-			itemState=itemStateString.split(",");
-			
-			
+
+		while (true) {
+
+			itemStateString = entrada.nextLine().toString();
+			itemState = itemStateString.split(",");
+
+			bulletManager(itemState[3].compareTo("true") == 0);
+
 			
 			
 		}
-		
+
+	}
+
+	private void bulletManager(Boolean b) {
+		// TODO crear un entity para la bala si recibo true, que esta tenga una duración
+		// de vida y compuebe si ha colisonado con otro objeto
+		// TODO CREAR ARRAY DE BALAS
 	}
 
 	public String getNombre() {
@@ -99,13 +97,15 @@ public class Connection extends Thread{
 	public String getSkin() {
 		return skin;
 	}
+
 	public int getIdentity() {
 		return identity;
 	}
+
 	public String getItemStateString() {
 		return itemStateString;
 	}
-	private String itemStateString; 
-	
+
+	private String itemStateString;
 
 }
