a56eb61649999bb60136a9eaee2b1c6fc048c8f2
Kevin David Cabrera Rodríguez <kevincabrerarodriguez@gmail.com>
2019-02-25 21:18:45 +0000

    Datos para el reporte del server

--- src/main/java/dad/javaspace/reports/ServerReportBean.java
+++ src/main/java/dad/javaspace/reports/ServerReportBean.java
@@ -1,10 +1,39 @@
 package dad.javaspace.reports;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class ServerReportBean {
 
 	private int disparos = 0;
-	private String ganador;
-	private String[] nombreJugadores;
+	private String ganador = "";
+	private ArrayList<String> nombreJugadores = new ArrayList<>();
+	private ArrayList<String> ranking = new ArrayList<>();
+	private LocalDate fechaInicio, fechaFin;
+
+	public ArrayList<String> getRanking() {
+		return ranking;
+	}
+
+	public void setRanking(ArrayList<String> ranking) {
+		this.ranking = ranking;
+	}
+
+	public LocalDate getFechaInicio() {
+		return fechaInicio;
+	}
+
+	public void setFechaInicio(LocalDate fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
+
+	public LocalDate getFechaFin() {
+		return fechaFin;
+	}
+
+	public void setFechaFin(LocalDate fechaFin) {
+		this.fechaFin = fechaFin;
+	}
 
 	public int getDisparos() {
 		return disparos;
@@ -22,11 +51,11 @@ public class ServerReportBean {
 		this.ganador = ganador;
 	}
 
-	public String[] getNombreJugadores() {
+	public ArrayList<String> getNombreJugadores() {
 		return nombreJugadores;
 	}
 
-	public void setNombreJugadores(String[] nombreJugadores) {
+	public void setNombreJugadores(ArrayList<String> nombreJugadores) {
 		this.nombreJugadores = nombreJugadores;
 	}
 
